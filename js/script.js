b0e4882990398532056a4810b4f81b84f92a7c23
YAYMobilya <YAYMobilyaa@gmail.com>
2025-07-20 00:00:56 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.sidebar > ul > li > a').forEach(function(el){
         el.addEventListener('click', function(e){
-            if(window.innerWidth <= 768){
+            if(window.innerWidth <= 1024){
                 e.preventDefault();
                 let parent = this.parentElement;
                 parent.classList.toggle('open');
