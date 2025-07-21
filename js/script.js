2470601b21915d413c721585f5964006aa4eb10f
YAYMobilya <YAYMobilyaa@gmail.com>
2025-07-21 23:03:18 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.sidebar > ul > li > a').forEach(function(el){
-        el.addEventListener('click', function(e){
-            if(window.innerWidth <= 1024 && this.nextElementSibling && this.nextElementSibling.classList.contains('submenu')){
-                e.preventDefault();
-                this.parentElement.classList.toggle('open');
-            }
-        });
-    });
-    const contactToggle = document.querySelector('#contact-toggle');
-    const contactPanel = document.querySelector('#contact-panel');
-    if(contactToggle){
-        contactToggle.addEventListener('click', function(e){
-            e.preventDefault();
-            contactPanel.classList.toggle('active');
-        });
-    }
-});
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+}
